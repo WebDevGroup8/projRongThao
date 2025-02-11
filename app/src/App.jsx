bc7cfc3dfd828929b443ecb2a80e668d6c7ab10b
SoravitSukkarn <soravit.sukkarn@gmail.com>
2025-02-11 13:37:35 +0700

    fix: build fail from vercel

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "./pages/Login";
 import { HomePage } from "./pages/HomePage";
 import { DashBoard } from "./pages/DashBoard";
-import ShoppingCart from "./pages/shoppingCart";
+import ShoppingCart from "./pages/ShoppingCart";
 import NavigationBar from "./components/NavBar";
 import ItemDetail from "./pages/ItemDetail";
 import { AuthProvider } from "./context/useAuth";
