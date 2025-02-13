d4ea3c4ca598d0dadc3f185cf8ecea97a0808d60
Worachat <worachat.smn@gmail.com>
2025-02-13 21:25:46 +0700

    feat: Order tag

--- app/src/pages/ViewOrder.jsx
+++ app/src/pages/ViewOrder.jsx
@@ -3,10 +3,11 @@ import { CircleCheck } from "lucide-react";
 import { Truck } from "lucide-react";
 import ax from "../conf/ax";
 import dayjs from "dayjs";
+import conf from "../conf/mainapi";
 
 export const TruckIcon = () => {
   return (
-    <div className="start-0 bg-white border-2 rounded-full w-fit h-fit p-1">
+    <div className="start-0 h-fit w-fit rounded-full border-2 bg-white p-1">
       <Truck />
     </div>
   );
@@ -17,8 +18,9 @@ export const GroupViewCard = (props) => {
 
   const fetchProducts = async () => {
     try {
-      const res = await ax.get(`/orders?populate=*`);
-      setOrderItems(res.data.data);
+      const res = await ax.get(`/users/me?populate=order_histories`);
+      setOrderItems(res.data.order_histories);
+      console.log(res.data.order_histories);
     } catch (error) {
       console.error("Error fetching products:", error);
     }
@@ -28,55 +30,78 @@ export const GroupViewCard = (props) => {
     fetchProducts();
   }, []);
 
+  const isPendingOrCanceled = ["Pending", "Abandoned", "Canceled"].includes(
+    orderItems?.orderStatus,
+  );
+
+  const status = {
+    Pending: "bg-yellow-100 text-yellow-800 border-yellow-400",
+    Paid: "bg-green-100 text-green-800 border-green-400",
+    Abandoned: "bg-red-100 text-red-800 border-red-400",
+    Completed: "bg-gray-100 text-gray-800 border-gray-400",
+    Shipped: "bg-blue-100 text-blue-800 border-blue-400",
+    Canceled: "bg-red-100 text-red-800 border-red-400",
+  };
   return (
     <div>
       {orderItems?.map((item) => (
         <div
           key={item.id}
-          className=" bg-white rounded-lg shadow-sm h transition-shadow  flex flex-col border border-gray-300 px-5 pt-2 pb-10 mt-5 lg:px-10 lg:pb-10"
+          className="mt-5 flex w-full flex-col rounded-lg border border-gray-300 bg-white px-5 pt-2 pb-10 shadow-sm transition-shadow lg:px-10 lg:pb-10"
         >
-          <div className="flex flex-col h-full lg:flex-row lg:pb-10 lg:pt-5">
-            <div className="flex flex-col relative lg:pe-90">
-              <div className="text-2xl lg:text-4xl whitespace-nowrap">
-                Order #{item.id}
-              </div>
-              <div className="text-xs whitespace-nowrap lg:text-sm lg:pt-2">
-                {dayjs(item.createdAt).format(
-                  "[Date ]DD MMMM YYYY [Time ]HH:mm a"
-                )}
+          <div className="flex h-full flex-col lg:flex-row lg:pt-5 lg:pb-10">
+            <div className="relative">
+              <div>
+                <div className="text-2xl whitespace-nowrap lg:text-4xl">
+                  Order{" "}
+                  <span className="text-xl lg:text-2xl">
+                    #{item.documentId}
+                  </span>
+                </div>
+                <div className="text-xs whitespace-nowrap lg:pt-2 lg:text-sm">
+                  {dayjs(item.createdAt).format(
+                    "[Date ]DD MMMM YYYY [Time ]HH:mm a",
+                  )}
+                </div>
+                <div
+                  className={`mt-3 h-7 w-30 rounded-2xl border-2 ${status[item?.orderStatus]} text-center`}
+                >
+                  {item?.orderStatus}
+                </div>
               </div>
             </div>
-            <div className="lg:justify-center flex flex-col relative w-full  h-full justify-center items-center mt-4 ">
-              <div className="flex w-60 lg:w-120 h-5 justify-center items-center">
+
+            <div className="relative mt-4 flex h-full w-full flex-col items-center justify-center lg:justify-center">
+              <div className="flex h-5 w-60 items-center justify-center lg:w-120">
                 {item.orderStatus === "Shipped" && (
-                  <hr className="transition-normal  border-5 rounded-2xl border-blue-700 w-full" />
+                  <hr className="w-full rounded-2xl border-5 border-blue-700 transition-normal" />
                 )}
 
                 {item.orderStatus === "Completed" ? (
-                  <hr className="transition-normal  border-5 rounded-2xl border-blue-700 w-full" />
+                  <hr className="w-full rounded-2xl border-5 border-blue-700 transition-normal" />
                 ) : (
-                  <hr className="border-5 rounded-2xl border-gray-200 w-full " />
+                  <hr className="w-full rounded-2xl border-5 border-gray-200" />
                 )}
               </div>
 
-              <div className="absolute flex flex-row justify-between items-center w-60 lg:w-120 top-1 ">
-                <div className="flex items-center justify-center w-3 h-3 bg-black rounded-full ">
-                  {item.orderStatus === "Pending" && <TruckIcon />}
+              <div className="absolute top-1 flex w-60 flex-row items-center justify-between lg:w-120">
+                <div className="flex h-3 w-3 items-center justify-center rounded-full bg-black">
+                  {item.orderStatus === "Paid" && <TruckIcon />}
                 </div>
-                <div className="flex items-center justify-center w-3 h-3 bg-black rounded-full ">
+                <div className="flex h-3 w-3 items-center justify-center rounded-full bg-black">
                   {item.orderStatus === "Shipped" && <TruckIcon />}
                 </div>
-                <div className="flex items-center justify-center w-3 h-3 bg-black rounded-full ">
+                <div className="flex h-3 w-3 items-center justify-center rounded-full bg-black">
                   {item.orderStatus === "Completed" && <TruckIcon />}
                 </div>
               </div>
 
               {item.orderStatus === "Canceled" ? (
-                <div className="text-red-500 text-l font-semibold underline">
+                <div className="text-l font-semibold text-red-500 underline">
                   Canceled
                 </div>
               ) : (
-                <div className="flex flex-row text-xs w-68 justify-between mt-2 lg:w-128">
+                <div className="mt-2 flex w-68 flex-row justify-between text-xs lg:w-128">
                   <div className="text-xs">Shipped</div>
                   <div className="text-xs">Out for Delivery</div>
                   <div className="text-xs">Delivery</div>
@@ -84,7 +109,7 @@ export const GroupViewCard = (props) => {
               )}
             </div>
           </div>
-          
+
           <ViewOrderCard item={item.order_product} />
         </div>
       ))}
@@ -94,37 +119,40 @@ export const GroupViewCard = (props) => {
 
 export const ViewOrderCard = ({ item }) => {
   return (
-    <div className="lg:flex lg:gap-0 lg:flex-wrap lg:justify-between lg:px-18 mt-2">
+    <div className="mt-2 lg:flex lg:flex-wrap lg:justify-between lg:gap-0 lg:px-18">
       {item?.map((item) => (
         <div
           key={item.id}
-          className=" w-full mt-2 gap-5 lg:w-110 lg:flex lg:justify-between"
+          className="mt-2 w-full gap-5 lg:flex lg:w-110 lg:justify-between"
         >
-          <div className="lg:flex-row lg:flex lg:w-full lg:justify-between bg-white rounded-lg shadow-sm hover:shadow-lg transition-shadow duration-200 flex flex-col border border-gray-100 px-5 pt-2 pb-1 ">
-            <div className="w-full flex flex-row  mb-5 mt-3">
+          <div className="flex flex-col rounded-lg border border-gray-100 bg-white px-5 pt-2 pb-1 shadow-sm transition-shadow duration-200 hover:shadow-lg lg:flex lg:w-full lg:flex-row lg:justify-between">
+            <div className="mt-3 mb-5 flex w-full flex-row">
               <img
-                src={item.image || "/placeholder.svg"}
+                src={
+                  `${conf.imageUrlPrefix}${item.image[0].url}` ||
+                  "/placeholder.svg"
+                }
                 alt={item.name}
-                className="w-16 h-16 object-cover rounded-md mb3 lg:w-18 lg:h-18"
+                className="mb3 h-16 w-16 rounded-md object-cover lg:h-18 lg:w-18"
               />
-              <div className="flex flex-row justify-between w-full pt-2 lg:pt-0 lg:ps-9">
-                <div className="flex flex-col ms-3 text-xs lg:me-7">
-                  <div className="font-semibold whitespace-nowrap truncate w-28 pb-2 lg:text-sm">
+              <div className="flex w-full flex-row justify-between pt-2 lg:ps-9 lg:pt-0">
+                <div className="ms-3 flex flex-col text-xs lg:me-7">
+                  <div className="w-28 truncate pb-2 font-semibold whitespace-nowrap lg:text-sm">
                     {item.name}
                   </div>
-                  <div className="font-thin font-xs lg:text-sm">
+                  <div className="font-xs font-thin lg:text-sm">
                     Color: {item.color}
                   </div>
-                  <div className="font-thin font-xs lg:text-sm">
+                  <div className="font-xs font-thin lg:text-sm">
                     Size: {item.size}
                   </div>
                 </div>
-                <div className="flex flex-col text-xs ps-8 w-full whitespace-nowrap truncate">
-                  <div className="font-semibold pb-2 lg:text-sm">
+                <div className="flex w-full flex-col truncate ps-8 text-xs whitespace-nowrap">
+                  <div className="pb-2 font-semibold lg:text-sm">
                     {" "}
                     {item.price} THB
                   </div>
-                  <div className="font-thin font-xs lg:text-sm">
+                  <div className="font-xs font-thin lg:text-sm">
                     total: {item.quantity}
                   </div>
                 </div>
@@ -165,14 +193,14 @@ export default function ViewOrder() {
   const [active, setActive] = useState("To Ship");
 
   return (
-    <div className="p-10 ">
-      <div className="flex flex-col relative w-full">
-        <div className="lg:text-5xl text-2xl font-semibold lg:mb-3 mb-1">
+    <div className="p-10">
+      <div className="relative flex w-full flex-col">
+        <div className="mb-1 text-2xl font-semibold lg:mb-3 lg:text-5xl">
           Your Order
         </div>
 
         <div className>
-          <hr className="absolute -bottom-2 left-0 right-0 border-2 border-blue-900" />
+          <hr className="absolute right-0 -bottom-2 left-0 border-2 border-blue-900" />
         </div>
       </div>
       {active === "To Ship" ? <GroupViewCard /> : <div>safjas</div>}
