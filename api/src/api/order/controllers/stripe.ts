b692b1b3e3ed2e584b19d24ee46af1693b4f37b8
SoravitSukkarn <soravit.sukkarn@gmail.com>
2025-02-13 18:52:46 +0700

    add: stripe webhook

---
+++ api/src/api/order/controllers/stripe.ts
@@ -0,0 +1,67 @@
+import { Context } from "koa";
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default {
+  async stripeWebhookHandler(ctx: Context) {
+    const sig = ctx.request.headers["stripe-signature"];
+
+    let event: Stripe.Event;
+
+    try {
+      const unparsedBody = ctx.request.body[Symbol.for("unparsedBody")];
+      event = stripe.webhooks.constructEvent(
+        unparsedBody as string,
+        sig as string,
+        process.env.STRIPE_WEBHOOK_SECRET as string
+      );
+
+      console.log("✅ Webhook verified Type:", event.type);
+    } catch (err) {
+      console.error(
+        "❌[stripeWebhookHandler]: Webhook signature verification failed:",
+        err
+      );
+      ctx.status = 400;
+      ctx.body = { error: "Webhook signature verification failed." };
+      return;
+    }
+    switch (event.type) {
+      case "checkout.session.completed":
+        console.log("⚠️ User abandoned checkout.");
+        await updateOrderStatus(event.data.object.id, "paid");
+        break;
+      case "checkout.session.expired":
+        console.log("⚠️ User abandoned checkout.");
+        await updateOrderStatus(event.data.object.id, "abandoned");
+        break;
+
+      case "payment_intent.canceled":
+        console.log("❌ Payment was canceled.");
+        await updateOrderStatus(event.data.object.id, "canceled");
+        break;
+
+      default:
+        console.log(`ℹ️ Unhandled event: ${event.type}`);
+    }
+
+    ctx.status = 200;
+    ctx.body = { received: true };
+  },
+};
+
+async function updateOrderStatus(orderId: string, status: string) {
+  try {
+    await strapi.db.query("api::order.order").update({
+      where: {
+        stripeId: orderId, // The condition to find the record
+      },
+      data: {
+        orderStatus: status, // The attributes you want to update
+      },
+    });
+    console.log(`✅ Order ${orderId} status updated to: ${status}`);
+  } catch (error) {
+    console.error("❌ Error updating order status:", error);
+  }
+}
