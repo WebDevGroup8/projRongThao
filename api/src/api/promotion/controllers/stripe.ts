0dc1b9ce0569e8af078b5e625ce3ad61f1e95210
SoravitSukkarn <soravit.sukkarn@gmail.com>
2025-02-18 16:17:04 +0700

    add: crud for coupon api

--- api/src/api/promotion/controllers/stripe.ts
+++ api/src/api/promotion/controllers/stripe.ts
@@ -3,6 +3,29 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default {
+  async queryAllPromotion(ctx: Context) {
+    try {
+      const coupons = await stripe.coupons.list();
+      ctx.status = 200;
+      ctx.body = coupons;
+    } catch (e) {
+      console.log("Create Promotion Coupon ", e);
+      ctx.status = 500;
+      ctx.body = { error: `Create Promotion Coupon ${e}` };
+    }
+  },
+  async queryOnePromotion(ctx: Context) {
+    const { id } = ctx.request.body;
+    try {
+      const coupon = await stripe.coupons.retrieve(id);
+      ctx.status = 200;
+      ctx.body = coupon;
+    } catch (e) {
+      console.log("Create Promotion Coupon ", e);
+      ctx.status = 500;
+      ctx.body = { error: `Create Promotion Coupon ${e}` };
+    }
+  },
   async createPromotion(ctx: Context) {
     const { name, duration, percent_off } = ctx.request.body;
     try {
@@ -13,6 +36,34 @@ export default {
       });
       ctx.status = 200;
       ctx.body = coupon;
+    } catch (e) {
+      console.log("Create Promotion Coupon ", e);
+      ctx.status = 500;
+      ctx.body = { error: `Create Promotion Coupon ${e}` };
+    }
+  },
+  async updatePromotion(ctx: Context) {
+    const { id, order_id } = ctx.request.body;
+    try {
+      const coupon = await stripe.coupons.update(id, {
+        metadata: {
+          order_id: order_id,
+        },
+      });
+      ctx.status = 200;
+      ctx.body = coupon;
+    } catch (e) {
+      console.log("Create Promotion Coupon ", e);
+      ctx.status = 500;
+      ctx.body = { error: `Create Promotion Coupon ${e}` };
+    }
+  },
+  async deletePromotion(ctx: Context) {
+    const { id } = ctx.request.body;
+    try {
+      const deleted = await stripe.coupons.del(id);
+      ctx.status = 200;
+      ctx.body = deleted;
     } catch (e) {
       console.log("Create Promotion Coupon ", e);
       ctx.status = 500;
--- api/src/api/promotion/routes/stripe.ts
+++ api/src/api/promotion/routes/stripe.ts
@@ -1,9 +1,46 @@
 export default {
   routes: [
     {
+      // Query All Coupon
+      method: "GET",
+      path: "/stripe/promotions",
+      handler: "stripe.queryAllPromotion",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      // Query One Coupon
+      method: "GET",
+      path: "/stripe/promotion/:id",
+      handler: "stripe.queryOnePromotion",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      // Create Coupon
       method: "POST",
-      path: "/promotion/create",
+      path: "/stripe/promotion",
       handler: "stripe.createPromotion",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      // Update Coupon
+      method: "POST",
+      path: "/stripe/promotion",
+      handler: "stripe.updatePromotion",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      // Delete Coupon
+      method: "DELETE",
+      path: "/stripe/promotion/:id",
+      handler: "stripe.deletePromotion",
       config: {
         policies: [],
       },
