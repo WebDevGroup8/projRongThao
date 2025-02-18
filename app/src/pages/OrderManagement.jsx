1e85052a213a9771dc2dc72a494664df33c49984
Worachat <worachat.smn@gmail.com>
2025-02-18 19:41:15 +0700

    Feat/order table (#84)
    * add: ordertable , search
    
    * add: edit function
    
    * add: PUT newStatus
    
    * add: Orders route
    
    * add: update table
    
    * delete: console.log

---
+++ app/src/pages/OrderManagement.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OrderTable from "../components/OrderTable";
+
+const OrderManagement = () => {
+  return (
+    <div>
+      <OrderTable configView={true} />
+    </div>
+  );
+};
+
+export default OrderManagement;
