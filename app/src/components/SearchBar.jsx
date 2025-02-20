eb9dbc9c9eed8edbb5ece45244bd0ae51d437303
Worachat <worachat.smn@gmail.com>
2025-02-20 21:01:39 +0700

    fix: search-bar margin

--- app/src/components/SearchBar.jsx
+++ app/src/components/SearchBar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const SearchBar = ({ onSearch }) => {
   return (
-    <div className="h-fit w-full max-w-md">
+    <div className="w-full max-w-md">
       <input
         type="search"
         placeholder="Search products..."
         onChange={(e) => onSearch(e.target.value)}
-        className="w-full rounded-md border border-gray-300 bg-white px-4 py-2 shadow-md focus:ring-2 focus:ring-gray-300 focus:outline-none"
+        className="mt-5 w-full rounded-md border border-gray-300 bg-white px-4 py-2 shadow-md focus:ring-2 focus:ring-gray-300 focus:outline-none"
       />
     </div>
   );
