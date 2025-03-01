16966c8060e77abd5787e544fbfb97a5cee2efaf
Nattakrit Machoo <dungmachoo@gmail.com>
2025-03-01 15:23:08 +0700

    update clickable card

--- app/src/components/public/home/ClickableCard.jsx
+++ app/src/components/public/home/ClickableCard.jsx
@@ -5,13 +5,19 @@ export default function ClickableCard(props) {
 
   return (
     <div
-      className="relative h-55 w-55 cursor-pointer rounded-xl bg-cover bg-center shadow-lg hover:scale-115"
-      style={{ backgroundImage: `url('${props.imageUrl}')` }}
+      className="relative h-55 w-55 cursor-pointer rounded-xl bg-cover shadow-lg overflow-hidden"
       onClick={() => navigate(props.navigateTo)}
     >
-      <div className="absolute inset-0 flex items-end rounded-xl bg-black/20 p-3">
+
+      <div
+        className="absolute inset-0 bg-cover bg-center transition-transform duration-300 hover:scale-115"
+        style={{ backgroundImage: `url('${props.imageUrl}')` }}
+      ></div>
+
+      <div className="absolute inset-0 flex items-end rounded-xl bg-black/20 p-3 pointer-events-none">
         <h2 className="text-sm font-bold text-white">{props.title}</h2>
       </div>
+
     </div>
   );
 }
