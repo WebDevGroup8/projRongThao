99e51dbbf709a6d0bb417bbb704c724e2257c123
Soravit Sukkarn <soravit.sukkarn@gmail.com>
2025-03-01 15:49:24 +0700

    add: invalid not found

--- app/src/components/layout/Otherwise.jsx
+++ app/src/components/layout/Otherwise.jsx
@@ -2,7 +2,7 @@ export default function Otherwise() {
   return (
     <div className="flex h-full min-h-screen w-full justify-center">
       <div className="mt-20 flex flex-col text-center">
-        <p className="text-4xl font-bold">Path not found</p>
+        <p className="text-4xl font-bold">Invalid not found</p>
         <a className="mt-4 text-xl hover:underline" href="/">
           Go Back
         </a>
