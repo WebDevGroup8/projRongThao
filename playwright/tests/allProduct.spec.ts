1a72b5e7c6161c6e9b934c4c43a08144ca51d4c2
Worachat <worachat.smn@gmail.com>
2025-03-05 15:35:38 +0700

    test: fetch data

--- playwright/tests/allProduct.spec.ts
+++ playwright/tests/allProduct.spec.ts
@@ -8,3 +8,15 @@ test("Should display product details correctly", async ({ page }) => {
     const product = page.locator('[data-testid="product-item"]').first();
     await expect(product).toBeVisible(); 
   });
+
+  test("Fetch Product correctly", async ({ page }) => {
+    await page.goto(BASE_URL); 
+
+    const response = await page.waitForResponse(async (response) => {
+        return response.url().includes("/api/products") && response.status() === 200;
+    });
+    const responseData = await response.json();
+    expect(responseData).toHaveProperty("data");
+    expect(responseData.data).toBeInstanceOf(Array);
+    expect(responseData.data.length).toBeGreaterThan(0);
+});
